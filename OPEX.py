bcb56358af30b233a5577211f3e13eb951aeb2be
elcacique69 <gonzalez.jcv1@gmail.com>
2023-08-03 17:45:56 +0200

    Update OPEX.py

--- OPEX.py
+++ OPEX.py
@@ -1,12 +1,34 @@
-# Library
 import pandas as pd
 
-def opex_20_DC(closing_date, insurance_percentage, agency_percentage, handling_percentage, storage_cost):
+def opex(closing_date,
+               Type,
+               insurance_fees, 
+               agency_fees,
+               bad_debt, 
+               handling_fees, 
+               storage_cost
+               ):
+    
+    """
+    Calculates the total operating expenses (OPEX) for containers of a specified type within a portfolio based on various factors
+    such as insurance fees, agency fees, bad debt, handling fees, and storage cost.
+
+    Parameters:
+    - closing_date (str): The date at which the function is supposed to calculate the OPEX. Expected format 'YYYY-MM-DD'.
+    - Type (str): The type of containers to calculate the OPEX for. Expected values are container types like "20'DC", "40'DC", "40'HC".
+    - insurance_fees, agency_fees, bad_debt, handling_fees (float): Different types of fees and costs associated with the containers. 
+      These values are expected to be in a decimal form representing the fraction of the life cycle revenues each fee takes up.
+    - storage_cost (float): The daily storage cost for a single container that is off lease.
+
+    Returns:
+    - total_opex (float): The total operating expenses (OPEX) for the specified type of containers at the given closing date.
+    """
+    
     # Read data from Excel file into a DataFrame
     df_portfolio = pd.read_excel('/Users/carlosjosegonzalezacevedo/Downloads/Data_Set_Closing (3).xlsx', sheet_name='Planned Portfolio')
 
-    # Filter the data to only include 20'DC containers
-    df_portfolio = df_portfolio[df_portfolio['Type'] == "20'DC"]
+    # Filter the data to only include specific type of containers
+    df_portfolio = df_portfolio[df_portfolio['Type'] == Type]
 
     # Convert dates to datetime
     df_portfolio['Manufacturing Date'] = pd.to_datetime(df_portfolio['Manufacturing Date'])
@@ -29,19 +51,40 @@ def opex_20_DC(closing_date, insurance_percentage, agency_percentage, handling_p
     df_portfolio['Life Cycle Revenues'] = df_portfolio['Annual Revenue'] * df_portfolio['Lifecycle Remaining Years']
     life_cycle_revenues = df_portfolio['Life Cycle Revenues'].sum()
 
+    # Calculate costs
+
+    ## INSURANCE FEE ##
+    insurance_cost = life_cycle_revenues * insurance_fees
+
+    ## AGENCY FEES ##
+    agency_cost = life_cycle_revenues * agency_fees
+
+    ## BAD DEBT ##
+    bad_debt_cost = life_cycle_revenues * bad_debt
+
+    ## HANDLING COSTS ##
+    handling_cost = life_cycle_revenues * handling_fees
+
+    ## STORAGE COSTS ##
+
     # Calculate Storage cost for Off lease containers
     df_off_lease = df_portfolio[df_portfolio['Current Status'] == 'Off Lease']
     number_of_units = len(df_off_lease)
     off_lease_period = 30
     total_storage_cost = number_of_units * (storage_cost * off_lease_period)
 
-    # Calculate costs
-    insurance_cost = life_cycle_revenues * insurance_percentage
-    agency_cost = life_cycle_revenues * agency_percentage
-    handling_cost = life_cycle_revenues * handling_percentage
+    total_opex = insurance_cost + agency_cost + bad_debt_cost + handling_cost + total_storage_cost
 
-    return(life_cycle_revenues, insurance_cost, agency_cost, handling_cost, total_storage_cost)
+    return(total_opex)
 
-revenues_OPEX = opex_20_DC('2023-06-12', 0.003, 0.007, 0.01, 0.55)
 
-print(revenues_OPEX)
+TFC_OPEX = opex('2023-06-12', "20'DC",  0.003, 0.007, 0.005, 0.02, 0.55)
+FFC_OPEX = opex('2023-06-12', "40'DC", 0.003, 0.007, 0.005, 0.02, 1.10)
+FHC_OPEX = opex('2023-06-12', "40'HC", 0.003, 0.007, 0.005, 0.02, 1.10)
+OPEX = TFC_OPEX + FFC_OPEX + FHC_OPEX
+
+
+print(f"The total 20'DC OPEX: {TFC_OPEX:,.2f} USD")
+print(f"The total 40'DC OPEX: {FFC_OPEX:,.2f} USD")
+print(f"The total 40'HC OPEX: {FHC_OPEX:,.2f} USD")
+print(f"The total OPEX: {OPEX:,.2f} USD")
