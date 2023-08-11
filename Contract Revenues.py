4e82abe0c01c6dca378ff7b7390dbde9cec79689
elcacique69 <gonzalez.jcv1@gmail.com>
2023-08-11 12:18:12 +0200

    Update Contract Revenues.py

--- Contract Revenues.py
+++ Contract Revenues.py
@@ -1,18 +1,35 @@
 import pandas as pd
-import numpy as np
 
-def calculate_quarterly_revenues(file_path, sheet_name):
-    df = pd.read_excel(file_path, sheet_name=sheet_name)
-    df['Closing Date'] = pd.to_datetime(df['Closing Date'])
-    df['End Contract Date'] = pd.to_datetime(df['End Contract Date'])
-    df['Contract Days'] = (df['End Contract Date'] - df['Closing Date']).dt.days
-
-    # Manejar los casos en los que Contract Days pueda ser cero o NA
-    df['Contract Quarters'] = np.where(
-        df['Contract Days'] > 0,
-        np.ceil(df['Contract Days'] / 90).astype(int),
-        0 # Valor predeterminado si Contract Days es <= 0
+def calculate_quarterly_revenues():
+    """
+    description
+    """
+    df_portfolio = pd.read_excel(
+        '/Users/carlosjosegonzalezacevedo/Documents/GitHub/DCF---Portfolio-Acquisition-Tool/Data_Set_Closing.xlsx',
+        sheet_name= 'Planned Portfolio'
     )
+
+    # Putting everything on date format
+
+    df_portfolio['Closing Date'] = pd.to_datetime(df_portfolio['Closing Date'])
+    df_portfolio['End Contract Date'] = pd.to_datetime(df_portfolio['End Contract Date'])
+
+    # Constants
+
+    quarter = 90  # days
+
+    # New Columns:
+
+    df_portfolio['Remaining Lease Term (Days)'] = \
+        (df_portfolio['End Contract Date'] - df_portfolio['Closing Date']).dt.days
+
+    df_portfolio['Quarterly Revenues'] = df_portfolio['Per Diem (Unit)'] * quarter
+
+    df_portfolio['Contra']
+
+    # Operations
+
+
 
     df['Total Revenue'] = df['Per Diem (Unit)'] * df['Contract Days']
     revenue_df = df[['Contract Number', 'Closing Date', 'Total Revenue', 'Contract Quarters']]
@@ -22,9 +39,9 @@ def calculate_quarterly_revenues(file_path, sheet_name):
     return quarterly_revenues
 
 # Ruta del archivo
-file_path = '/Users/carlosjosegonzalezacevedo/Downloads/Data_Set_Closing (3).xlsx'
+file_path = '/Users/carlosjosegonzalezacevedo/Documents/GitHub/DCF---Portfolio-Acquisition-Tool/Data_Set_Closing.xlsx'
 sheet_name = 'Planned Portfolio'
 
 # Ejemplo de uso
-quarterly_revenues = calculate_quarterly_revenues(file_path, sheet_name)
+quarterly_revenues = calculate_quarterly_revenues()
 print(quarterly_revenues)
