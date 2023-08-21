b5c5266bb9f8f195dc404e16b9deb13256bb938b
elcacique69 <gonzalez.jcv1@gmail.com>
2023-08-21 12:11:07 +0200

    Update Correlation.py

--- Correlation.py
+++ Correlation.py
@@ -1,20 +1,24 @@
 # Library
 import pandas as pd
 
+# load data frame
+file_path = '/Users/carlosjosegonzalezacevedo/Documents/GitHub/DCF---Portfolio-Acquisition-Tool/Coorelation.xlsx'
+df = pd.read_excel(file_path)
 
-df = \
-    pd.read_excel('/Users/carlosjosegonzalezacevedo/Documents/GitHub/DCF---Portfolio-Acquisition-Tool/Coorelation.xlsx')
-
-# Calcular la tasa de cambio del PD
+# Calculate the PD Change
 df["PD Change"] = df["PD"].diff()
 
-# Calcular la correlación entre el aumento del PD y las otras variables
-correlation_with_pd_change = df[["PD Change",
-                                 "Used Containers Price",
-                                 "New Container Price",
-                                 "Replacement",
-                                 "Operating Ratio",
-                                 "World GDP"
-                                 ]].corr()
+# Select columns for correlation calculation
+columns_for_correlation = [
+    "PD Change",
+    "Used Containers Price",
+    "New Container Price",
+    "Replacement",
+    "Operating Ratio",
+    "World GDP"
+]
+
+# Calculate correlation matrix
+correlation_with_pd_change = df[columns_for_correlation].corr()
 
 print(correlation_with_pd_change)
